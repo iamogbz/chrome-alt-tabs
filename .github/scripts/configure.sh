d9518c3fd2a825d2170a89094a1f783b9f90292f
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-04-21 23:36:45 -0400

    chore: new oss script

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/iamogbz/oss-boilerplate ../$REPO_NAME
+git clone https://github.com/iamogbz/node-js-boilerplate ../$REPO_NAME
 cd ../$REPO_NAME
 git remote set-url origin $REPO_URL
 make upstream
