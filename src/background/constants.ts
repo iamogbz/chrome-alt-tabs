eb0ceea9dffb21e2f17331a2bfb7a4f2e6c04466
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-12-02 09:22:30 -0500

    refactor: fix commands type hint

--- src/background/constants.ts
+++ src/background/constants.ts
@@ -1,7 +1,7 @@
 const prefixCommand = (command: string): string => `tabs-move-${command}`;
 const commandKeys = ["out", "back", "next", "prev"];
 export const COMMANDS: { [key: string]: string } = commandKeys.reduce(
-    (commands: string[], key: string) =>
+    (commands: {}, key: string) =>
         Object.assign(commands, { [key.toUpperCase()]: prefixCommand(key) }),
     {},
 );
