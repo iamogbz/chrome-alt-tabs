fc3dd013fa2a8a653898f751bbaa74878104c41b
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2021-02-24 00:33:11 -0500

    chore: remove obsolete prettier ts lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
   ],
   env: {
     browser: true,
